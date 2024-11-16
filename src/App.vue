3bad79e566f2ddf46f03e8d180ebd3af9ea754bc
prakhartiwari0 <65062036+prakhartiwari0@users.noreply.github.com>
2024-11-16 14:43:58 +0530

    Making it a nice beautiful page

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,23 @@
 <template>
-  <div class="w-full min-h-screen bg-gray-100 flex items-center justify-center p-4">
-    <img src="../src/assets/expl_logo.png"
-         alt="Placeholder"
-         class="max-w-full h-auto rounded-3xl shadow-lg hover:shadow-xl transition-shadow duration-300">
+  <div class="w-full h-screen bg-gray-100 flex flex-col items-center justify-center p-4 relative overflow-hidden">
+    <img src="../src/assets/flowers_decor_uptodown.png"
+         alt="Top right flower"
+         class="absolute top-0 right-0 w-72 md:w-96 z-0">
+
+    <img src="../src/assets/dandelions_purple_transparents.png"
+         alt="Bottom left flower"
+         class="absolute bottom-0 left-0 w-56 md:w-[30rem] z-0">
+
+    <div class="text-center z-10 space-y-4 p-2">
+      <h1
+          class="font-satisfy text-6xl md:text-8xl leading-tight bg-gradient-to-r from-[#9370DB] via-[#8B6FC9] to-[#6d53fd] text-transparent bg-clip-text p-2">
+        Explolearners
+      </h1>
+      <p
+         class="font-satisfy text-2xl md:text-3xl bg-gradient-to-r from-[#8B6FC9] via-[#7B68EE] to-[#9370DB] text-transparent bg-clip-text">
+        A World for Curiosity to Flourish
+      </p>
+    </div>
   </div>
 </template>
 
@@ -11,3 +26,9 @@ export default {
   name: 'App'
 }
 </script>
+
+<style>
+.font-satisfy {
+  font-family: 'Satisfy', cursive;
+}
+</style>
