159b1ebcd915d4814716c4fd6afcc2c6dada2587
prakhartiwari0 <65062036+prakhartiwari0@users.noreply.github.com>
2024-11-16 10:26:54 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="w-full min-h-screen bg-gray-100 flex items-center justify-center p-4">
+    <img src="../src/assets/expl_logo.png"
+         alt="Placeholder"
+         class="max-w-full h-auto rounded-3xl shadow-lg hover:shadow-xl transition-shadow duration-300">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
